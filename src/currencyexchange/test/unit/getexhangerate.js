22b6e847832cffded51255a9b67f3dcd12811dcb
Grant Steinfeld <Grant.Steinfeld.Tech@gmail.com>
2019-11-10 21:17:00 -0500

    3 smoke test passing

--- src/currencyexchange/test/unit/getexhangerate.js
+++ src/currencyexchange/test/unit/getexhangerate.js
@@ -8,8 +8,21 @@ describe("Get all currency exchange rates", () => {
     var currencyRateByCountryData = getCurrencyExchangeRates();
     currencyRateByCountryData.then(data => {
       assert(data.rates !== null, "Data should have a Dict called 'rates'");
-      //assert(Object.keys(data.rates).length === 31), "Should have x number of rate entries"
       assert(data.base === "EUR", "base rate should be EUR");
+      done();
+    }).catch(err => {
+      done(err);
+    });
+  });
+});
+
+describe("Expect non empty list of 32 for all currency exchange rates", () => {
+  it("should return all 3 letter country codes with currency for the day", done => {
+    var currencyRateByCountryData = getCurrencyExchangeRates();
+    currencyRateByCountryData.then(data => {
+      var numRates = Object.keys(data.rates).length;
+      console.log(`returned number of rate entries ${numRates}`);
+      assert(numRates == 32, "Should have 32 number of rate entries");
       done();
     }).catch(err => {
       done(err);
@@ -37,6 +50,8 @@ describe("Get a specific rate for a specific country code that does exist, i.e. 
     var currencyRateByCountryData = getCurrencyExchangeRate("USD");
     currencyRateByCountryData.then(data => {
       assert(isNaN(data) === false, "expect daily rate as a number");
+      var res = isNaN(parseFloat(data));    // test float -> https://stackoverflow.com/questions/12467542/how-can-i-check-if-a-string-is-a-float
+      assert( res === false, "expect daily rate as a float"); 
       done();
     }).catch(err => {
       assert()
