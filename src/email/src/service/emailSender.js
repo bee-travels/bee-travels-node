74a46d61ca85ebf9d850fb5fd9501f6c75f436d6
Mofizur Rahman <moficodes@gmail.com>
2019-11-22 11:16:41 -0500

    added sendgrid code

--- src/email/src/service/emailSender.js
+++ src/email/src/service/emailSender.js
@@ -1,7 +1,20 @@
+var sgMail = require("@sendgrid/mail");
+sgMail.setApiKey(process.env.SENDGRIP_API_KEY);
 
-function sendEmail(email, body){
+function sendEmail(email, subject, body){
   // send an email
   // send ok or fail
+  var from = process.env.EMAIL_ADDRESS;
+
+  const msg = {
+    to: email,
+    from: from,
+    subject: subject,
+    text: body
+  }
+
+  sgMail.send(msg);
+
 }
 
 export default sendEmail;
