c8b4285aea63db5da24dcc738a5148efe2f8bffa
Mofizur Rahman <moficodes@gmail.com>
2019-11-25 16:23:07 -0500

    added travis lint test

--- src/email/src/routes/email.js
+++ src/email/src/routes/email.js
@@ -9,9 +9,9 @@ var router = Router();
 /* GET list of destination locations */
 router.post("/", function (req, res) {
   // Grab the email and body of the person to send message to from the body
-  var email = "test@email.com";
-  var body = "hello hi";
-  var subject = "booking";
+  var email = req.body.email;
+  var body = req.body.body;
+  var subject = req.body.subject;
   sendEmail(email, subject, body);
   res.send("Done!");
 });
