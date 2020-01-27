cc230192fe2e2c85f2738dd83f2fcef26545fc8c
Nick <bourdakos1@gmail.com>
2020-01-27 15:08:41 -0500

    fix jpg

--- src/ui/client/src/components/Content.js
+++ src/ui/client/src/components/Content.js
@@ -114,8 +114,13 @@ const DestinationFragment = ({ destination }) => {
           style="mapbox://styles/mapbox/streets-v9"
           zoom={[7]}
           containerStyle={{
-            height: "237px",
-            width: "100%"
+            // height: "237px",
+            height: "261px",
+            // width: "100%",
+            position: "absolute",
+            bottom: 0,
+            left: 0,
+            right: 0
           }}
           center={{
             lng: destination.lng,
