0c20fb860f572b24f1b911af3c5423aa786ada7e
Mofizur Rahman <moficodes@gmail.com>
2020-02-11 00:44:14 -0500

    branch logic

--- .travis/build-and-push.sh
+++ .travis/build-and-push.sh
@@ -9,12 +9,12 @@ fi
 
 echo "prefix set to $prefix"
 
-# docker build -t beetravels/destination:${prefix}-$TRAVIS_COMMIT src/destination
-# docker build -t beetravels/hotel:${prefix}-$TRAVIS_COMMIT src/hotel
-# docker build -t beetravels/currencyexchange:${prefix}-$TRAVIS_COMMIT src/currencyexchange
-# docker build -t beetravels/ui:${prefix}-$TRAVIS_COMMIT src/ui
-# echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-# docker push beetravels/destination:${prefix}-$TRAVIS_COMMIT
-# docker push beetravels/hotel:${prefix}-$TRAVIS_COMMIT
-# docker push beetravels/currencyexchange:${prefix}-$TRAVIS_COMMIT
-# docker push beetravels/ui:${prefix}-$TRAVIS_COMMIT
+docker build -t beetravels/destination:${prefix}-$TRAVIS_COMMIT src/destination
+docker build -t beetravels/hotel:${prefix}-$TRAVIS_COMMIT src/hotel
+docker build -t beetravels/currencyexchange:${prefix}-$TRAVIS_COMMIT src/currencyexchange
+docker build -t beetravels/ui:${prefix}-$TRAVIS_COMMIT src/ui
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+docker push beetravels/destination:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/hotel:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/currencyexchange:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/ui:${prefix}-$TRAVIS_COMMIT
