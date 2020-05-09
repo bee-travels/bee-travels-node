c3d70aeec757b5c83dc060223241344ae646eda1
Nick <bourdakos1@gmail.com>
2020-05-09 18:42:32 -0400

    get all tests passing

--- services/destination-v1/src/services/dataHandler.test.js
+++ services/destination-v1/src/services/dataHandler.test.js
@@ -1,27 +1,63 @@
 import { describe, it } from "mocha";
-import { expect } from "chai";
+import chai, { expect } from "chai";
+import chaiJsonSchema from "chai-json-schema";
+
+chai.use(chaiJsonSchema);
 
 const { getCities, getCity } = require("./dataHandler");
+
+const destinationSchema = {
+  title: "destination schema v1",
+  type: "object",
+  required: [
+    "city",
+    "country",
+    "description",
+    "latitude",
+    "longitude",
+    "population",
+    "images",
+  ],
+  properties: {
+    city: { type: "string" },
+    country: { type: "string" },
+    description: { type: "string" },
+    latitude: { type: "number" },
+    longitude: { type: "number" },
+    population: { type: "number" },
+    images: {
+      type: "array",
+      minItems: 3,
+      items: {
+        type: "string",
+      },
+    },
+  },
+};
+
+const cityListSchema = {
+  title: "city list schema v1",
+  type: "array",
+  items: {
+    type: "object",
+    required: ["city", "country"],
+    properties: {
+      city: { type: "string" },
+      country: { type: "string" },
+    },
+  },
+};
 
 describe("getCities", () => {
   it("returns all cities", async () => {
     const data = await getCities();
-    expect(data.length).to.equal(183);
+    expect(data).to.have.jsonSchema(cityListSchema);
   });
 });
 
 describe("getCity", () => {
   it("returns city given valid country and city name", async () => {
     const data = await getCity("united-states", "new-york");
-    const expected = {
-      city: "New York",
-      country: "United States",
-      description:
-        "The City of New York, usually called either New York City (NYC) or simply New York (NY), is the most populous city in the United States. With an estimated 2018 population of 8,398,748 distributed over a land area of about 302.6 square miles (784 km2), New York is also the most densely populated major city in the United States. Located at the southern tip of the state of New York, the city is the center of the New York metropolitan area, the largest metropolitan area in the world by urban landmass and one of the world's most populous megacities, with an estimated 19,979,477 people in its 2018 Metropolitan Statistical Area and 22,679,948 residents in its Combined Statistical Area. A global power city, New York City has been described as the cultural, financial, and media capital of the world, and exerts a significant impact upon commerce, entertainment, research, technology, education, politics, tourism, art, fashion, and sports. The city's fast pace has inspired the term New York minute. Home to the headquarters of the United Nations, New York is an important center for international diplomacy.",
-      lat: "40.6943",
-      lng: "-73.9249",
-      population: "19354922",
-    };
-    expect(data).to.deep.equal(expected);
+    expect(data).to.have.jsonSchema(destinationSchema);
   });
 });
