2bbc35f49f212af7a8118d4a76266f848ea85fef
Mofizur Rahman <moficodes@gmail.com>
2020-05-11 19:06:08 -0400

    update travis build script, remove swagger yaml copy

--- .travis/build-and-push.sh
+++ .travis/build-and-push.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-if [ ${TRAVIS_BRANCH} == development ]; then
-  prefix=dev
-else
-  prefix=${TRAVIS_BRANCH}
-fi
-
+prefix=node
 echo "prefix set to $prefix"
 
 docker build -t bee-travels/node-base:v1 .
 
-docker build -t beetravels/destination:${prefix}-$TRAVIS_COMMIT services/destination
-docker build -t beetravels/hotel:${prefix}-$TRAVIS_COMMIT services/hotel
+docker build -t beetravels/destination-v1:${prefix}-$TRAVIS_COMMIT services/destination-v1
+docker build -t beetravels/destination-v2:${prefix}-$TRAVIS_COMMIT services/destination-v2
+docker build -t beetravels/carrental-v2:${prefix}-$TRAVIS_COMMIT services/carrental-v2
+docker build -t beetravels/hotel-v1:${prefix}-$TRAVIS_COMMIT services/hotel-v1
+docker build -t beetravels/hotel-v1:${prefix}-$TRAVIS_COMMIT services/hotel-v2
 docker build -t beetravels/currencyexchange:${prefix}-$TRAVIS_COMMIT services/currency-exchange
 docker build -t beetravels/ui:${prefix}-$TRAVIS_COMMIT services/ui
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push beetravels/destination:${prefix}-$TRAVIS_COMMIT
-docker push beetravels/hotel:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/destination-v1:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/destination-v2:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/carrental-v2:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/hotel-v1:${prefix}-$TRAVIS_COMMIT
+docker push beetravels/hotel-v2:${prefix}-$TRAVIS_COMMIT
 docker push beetravels/currencyexchange:${prefix}-$TRAVIS_COMMIT
 docker push beetravels/ui:${prefix}-$TRAVIS_COMMIT
