d732b5d62154d8d1083b4c07bb7a2d7220ce2328
Nick <bourdakos1@gmail.com>
2020-05-12 14:33:20 -0400

    try out fancy feast

--- services/currency-exchange/src/routes/currency.js
+++ services/currency-exchange/src/routes/currency.js
@@ -6,6 +6,14 @@ import CurrencyNotFoundError from "../errors/CurrencyNotFoundError";
 
 const router = Router();
 
+/**
+ * GET /api/v1/currency/rates
+ * @description Get a list of all exchange rates.
+ * @queryParam {[string]} from - TODO.
+ * @response 200 - TODO.
+ * @response 500 - TODO.
+ * @response 400 - TODO.
+ */
 router.get("/rates", async (_, res, next) => {
   try {
     const data = await getExchangeRates();
@@ -15,19 +23,30 @@ router.get("/rates", async (_, res, next) => {
   }
 });
 
+/**
+ * GET /api/v1/currency/convert/{from}/{to}
+ * @description Get exchange rate for converting one currency to another.
+ * @pathParam {string} from - TODO.
+ * @pathParam {string} to - TODO.
+ */
 router.get("/convert/:from/:to", async (req, res, next) => {
   const { from, to } = req.params;
   try {
     const data = await convert(from, to);
-    res.json({ rate: data });
+    return res.json({ rate: data });
   } catch (e) {
     if (e instanceof CurrencyNotFoundError) {
       return res.status(400).json({ error: e.message });
     }
-    next(e);
+    return next(e);
   }
 });
 
+/**
+ * GET /api/v1/currency/{code}
+ * @description TODO.
+ * @pathParam {string} code - TODO.
+ */
 router.get("/:code", async (req, res, next) => {
   const { code } = req.params;
   try {
@@ -37,14 +56,19 @@ router.get("/:code", async (req, res, next) => {
     if (e instanceof CurrencyNotFoundError) {
       return res.status(400).json({ error: e.message });
     }
-    next(e);
+    return next(e);
   }
 });
 
+/**
+ * GET /api/v1/currency
+ * @description TODO.
+ * @queryParam {string} country - TODO.
+ */
 router.get("/", async (req, res, next) => {
   const { country } = req.query;
   if (country === undefined) {
-    next();
+    return next();
   }
   try {
     const data = await getCountry(country);
@@ -53,7 +77,7 @@ router.get("/", async (req, res, next) => {
     if (e instanceof CountryNotFoundError) {
       return res.status(400).json({ error: e.message });
     }
-    next(e);
+    return next(e);
   }
 });
 
